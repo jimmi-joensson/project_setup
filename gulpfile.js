d3b508c328ff1ac9c9aa4adea5d71fca0878bd02
Jimmi Schmidt Jønsson Hansen <jimmi@uncutt.dk>
2016-02-23 13:01:01 +0100

    gulpfile.js optimized with error handling

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,70 @@ var gulpsass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var gutil = require('gulp-util');
 var browsersync = require('browser-sync').create();
 
-function errorLog(error) {
-    console.error.bind(console);
+var reportError = function (error) {
+    var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
+
+    notify({
+        title: 'Task Failed [' + error.plugin + ']',
+        message: lineNumber + 'See console.',
+        sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+    }).write(error);
+
+    gutil.beep(); // Beep 'sosumi' again
+
+    // Inspect the error object
+    //console.log(error);
+
+    // Easy error reporting
+    //console.log(error.toString());
+
+    // Pretty error reporting
+    var report = '';
+    var chalk = gutil.colors.white.bgRed;
+
+    report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+    report += chalk('PROB:') + ' ' + error.message + '\n';
+    if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+    if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+    console.error(report);
+
+    // Prevent the 'watch' task from stopping
     this.emit('end');
-}
+};
 
 gulp.task('sass', function() {
-  gulp.src('my_project/assets/sass/i.scss')
-      .pipe(gulpsass()
-          .on('error', gulpsass.logError))
-      .pipe(cssnano())
+  return gulp.src('my_project/assets/sass/i.scss')
+      .pipe(plumber({
+          errorHandler: reportError
+      }))
+      .pipe(sourcemaps.init())
+        .pipe(gulpsass())
+        .pipe(cssnano())
+      .pipe(sourcemaps.write())
+      .pipe(plumber.stop())
       .pipe(gulp.dest('my_project/css'))
       .pipe(browsersync.stream());
 });
 
 gulp.task('js', function() {
-    gulp.src([
+    return gulp.src([
         'bower_components/jquery/dist/jquery.js',
         'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
         'my_project/assets/js/main.js'
     ])
-    .pipe(concat('main.js'))
-    .pipe(uglify())
+        .pipe(plumber({
+            errorHandler: reportError
+        }))
+    .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('my_project/js'));
 });
 
--- my_project/index.php
+++ my_project/index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <div>Welcome to our test</div>
 </body>
 </html>
